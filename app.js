c1f5ef8b214e4e8645143e09ffb6881b682a2e57
Anghelo Flores <anghelogfp@gmail.com>
2025-03-09 21:50:21 -0500

    commit inicial

---
+++ app.js
@@ -0,0 +1,5 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+console.log("Hola, bienvenido a la red social de amigos");
+console.log(amigos);
