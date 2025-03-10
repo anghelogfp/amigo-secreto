edd4183a0b0c5ec1e247c6d332e0d17f2c2bf0d6
Anghelo Flores <anghelogfp@gmail.com>
2025-03-09 22:00:25 -0500

    agregado validar la entrada y agregar amigo al array

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-console.log("Hola, bienvenido a la red social de amigos");
 console.log(amigos);
 
 function agregarAmigo() {
     let amigo = document.getElementById("amigo").value;
+
+    if (!amigo) {
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+    
     amigos.push(amigo);
     console.log(amigos);
 }
